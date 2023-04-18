b2a2785c5941ff9dcf44ff427e960002c731b154
Giovane Fernandes <giosofer52@gmail.com>
2023-04-18 10:58:15 -0300

    Update verify_backup-daily.sh

--- verify_backup-daily.sh
+++ verify_backup-daily.sh
@@ -9,6 +9,7 @@
 #Put the path to backups directory here
 cd /home/backup-daily/
 
+#-mmin -480, less than 480 minutes, i used this paramenter to my case
 new_backup_name=$(find . -mmin -480 -name "*.tar.gz" | sort -nr | sed '1!d' | awk {'print $1'} | sed 's|^./||')
 old_backup_name=$(find . -mmin -480 -name "*.tar.gz" | sort -nr | sed '2!d' | awk {'print $1'} | sed 's|^./||')
 
@@ -18,10 +19,10 @@ old_backup_size=$(find . -name $old_backup_name -exec du {} + | awk {'print $1'}
 function backup_compare(){
 	if [ $new_backup_size -gt $old_backup_size ]
 	then 
-    echo "sucess"
+   		echo "sucess"
 	
 	else 
-    echo "error"
+    		echo "error"
 	fi	
 }
 
