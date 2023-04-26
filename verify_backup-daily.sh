d60938e650b792733ea8d7a7f39b9e4d12753623
Giovane Fernandes <giosofer52@gmail.com>
2023-04-26 09:30:01 -0300

    Implementation to save the log

--- verify_backup-daily.sh
+++ verify_backup-daily.sh
@@ -2,21 +2,25 @@
 #
 #author: Giovane Fernandes
 #description: Backup Verification Routine
-#version: 1.0
+#version: 1.0.1
 #04/13/2023 16:17
 #
 
-#Put the path to backups directory here
-cd /home/backup-daily/
+function verify_backup(){
+	#Put the path to backups directory here
+	cd /home/backup-daily/
 
-#-mmin -480, less than 480 minutes, i used this paramenter to my case
-new_backup_name=$(find . -mmin -480 -name "*.tar.gz" | sort -nr | sed '1!d' | awk {'print $1'} | sed 's|^./||')
-old_backup_name=$(find . -mmin -480 -name "*.tar.gz" | sort -nr | sed '2!d' | awk {'print $1'} | sed 's|^./||')
+	#-mmin -480, less than 480 minutes, i used this paramenter to my case
+	new_backup_name=$(find . -mmin -480 -name "*.tar.gz" | sort -nr | sed '1!d' | awk {'print $1'} | sed 's|^./||')
+	old_backup_name=$(find . -mmin -480 -name "*.tar.gz" | sort -nr | sed '2!d' | awk {'print $1'} | sed 's|^./||')
 
-new_backup_size=$(find . -name $new_backup_name -exec du {} + | awk {'print $1'})
-old_backup_size=$(find . -name $old_backup_name -exec du {} + | awk {'print $1'})
-
-function backup_compare(){
+	new_backup_size=$(find . -name $new_backup_name -exec du {} + | awk {'print $1'})
+	old_backup_size=$(find . -name $old_backup_name -exec du {} + | awk {'print $1'})
+	
+	#Print the variables
+	echo $new_backup_name $new_backup_size "-" $old_backup_name $old_backup_size 
+	
+	#Compare new_backup_size and old_backup_size
 	if [ $new_backup_size -gt $old_backup_size ]
 	then 
    		echo "sucess"
@@ -26,6 +30,8 @@ function backup_compare(){
 	fi	
 }
 
-#Call and Excute the function
-backup_compare 
+#Current date variable
+DATE=$(date +%y-%m-%d_%H%M)
 
+#Call, Excute the function and save output as log file
+verify_backup > /home/logs/log_${DATE}.out 2> /home/logs/log_${DATE}.err
